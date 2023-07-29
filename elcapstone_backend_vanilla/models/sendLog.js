85df2ba3eb0c92074095f6b100869c9097b73d7e
Andrew <mullinsa428@gmail.com>
2023-07-28 21:55:04 -0400

    backend up to date

--- elcapstone_backend_vanilla/models/sendLog.js
+++ elcapstone_backend_vanilla/models/sendLog.js
@@ -14,7 +14,6 @@ const sendLogSchema = new Schema(
         },
         grade: {
             type: String,
-            required: true,
         },
         description: {
             type: String,
